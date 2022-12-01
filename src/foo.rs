570eb7608e127de39dc32bd865205133c702f382
Ilya Bogdanov <fumlead@gmail.com>
2022-12-01 19:29:06 +0400

    Initial commit

---
+++ src/foo.rs
@@ -0,0 +1,9 @@
+pub fn give_answer() -> i32 {
+    42
+}
+
+mod bar {
+    fn functon_from_bar() -> i32 {
+        10
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use otus_modules_demo::prelude::*;
+
+fn main() {
+    println!("Hello, world!");
+    println!("{}", public_api());
+}
